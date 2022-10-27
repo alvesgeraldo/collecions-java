083056f52b9cc1e2bd657adbcaa0aa895e9ccf28
alvesgeraldo <jgalvesb@gmail.com>
2022-10-27 19:28:10 -0300

    Comparator de String

--- List/InterfaceComparable/src/App.java
+++ List/InterfaceComparable/src/App.java
@@ -13,17 +13,27 @@ public class App {
         meusGatos.add(new Gato("Jon", 18, "Tigrado"));
 
         // Apresentando a List por ordem de inserção
+        System.out.println("Ordem de Inserção");
         System.out.println(meusGatos);
 
         // Apresentando com ordenação aleatória
         Collections.shuffle(meusGatos);
+        System.out.println("Ordem Aleatória");
         System.out.println(meusGatos);
 
         // Apresentando por ordem de nome
         Collections.sort(meusGatos);
+        System.out.println("Ordem de Nome");
         System.out.println(meusGatos);
 
         // Apresentando por ordem de idade
-        
+        meusGatos.sort(new ComparatorIdade());
+        System.out.println("Ordem de Idade");
+        System.out.println(meusGatos);
+
+        // Apresentando por ordem de Cor
+        Collections.sort(meusGatos, new ComparatorCor());
+        System.out.println("Ordem de Cor");
+        System.out.println(meusGatos);
     }
 }
