3102a763b7bd802c45d378182928648ea5cc7922
alvesgeraldo <jgalvesb@gmail.com>
2022-10-28 16:32:56 -0300

    Metodo hasCode e equals

--- Set/InterfaceComparable/src/Serie.java
+++ Set/InterfaceComparable/src/Serie.java
@@ -32,5 +32,44 @@ public class Serie implements Comparable<Serie>{
     
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+    result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+    result = prime * result + ((tempoEpisodio == null) ? 0 : tempoEpisodio.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Serie other = (Serie) obj;
+    if (nome == null) {
+      if (other.nome != null)
+        return false;
+    } else if (!nome.equals(other.nome))
+      return false;
+    if (genero == null) {
+      if (other.genero != null)
+        return false;
+    } else if (!genero.equals(other.genero))
+      return false;
+    if (tempoEpisodio == null) {
+      if (other.tempoEpisodio != null)
+        return false;
+    } else if (!tempoEpisodio.equals(other.tempoEpisodio))
+      return false;
+    return true;
+  }
+
+  
+
   
 }
